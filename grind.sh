33caa3fbc5da3f9513c411a7b48b0ffd2adf380f
1313ou <1313ou@gmail.com>
2025-06-18 17:37:19 +0200

    bump new version

--- grind.sh
+++ grind.sh
@@ -27,5 +27,5 @@ echo "OUT:   ${OUTFILE}" 1>&2;
 
 opts="-pretty"
 
-jar=target/wndb2json-2.3.1-uber.jar
+jar=target/wndb2json-2.3.2-uber.jar
 java -ea -jar "${jar}" ${opts} "${IN}" "${IN2}" "${OUTFILE}"
